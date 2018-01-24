a9be8d20ddf1c92f7914b17adec1c9c8752fe8ed
Zach Scheck <zach.scheck@gmail.com>
2018-01-24 11:23:19 -0800

    redis enabled heroku

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const morgan = require('morgan');
 const axios = require('axios');
 const env = require('dotenv').config();
 const randomWords = require('random-words');
-// const redis = require('redis');
-
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 // const REDIS_PORT = process.env.REDIS_PORT;
 // const REDIS_HOST = process.env.REDIS_HOST;
 // const redisClient = redis.createClient(REDIS_HOST, REDIS_PORT);
@@ -15,9 +15,9 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-// redisClient.on('connect', () => {
-//   console.log('connected');
-// });
+redisClient.on('connect', () => {
+  console.log('connected');
+});
 
 app.get('/newword', (req, res) => {
 //   axios.get(`http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${process.env.WORDNIK_API_KEY}`)
@@ -30,17 +30,17 @@ app.get('/newword', (req, res) => {
 
 app.get('/synonyms/:word', (req, res) => {
   const word = req.params.word.toLowerCase();
-//   redisClient.exists(`${word}_word`, (err, reply) => {
-//     if (reply === 1) {
-//       console.log('true');
-//       redisClient.hgetall(`${word}_word`, (erro, object) => {
-//         //   console.log(object);
-//         let synonym = object.synonyms;
-//         synonym = synonym.split(',');
-//         // console.log(synonym);
-//         res.send(synonym);
-//       });
-//     } else {
+  redisClient.exists(`${word}_word`, (err, reply) => {
+    if (reply === 1) {
+      console.log('true');
+      redisClient.hgetall(`${word}_word`, (erro, object) => {
+        //   console.log(object);
+        let synonym = object.synonyms;
+        synonym = synonym.split(',');
+        // console.log(synonym);
+        res.send(synonym);
+      });
+    } else {
         console.log('false');
       axios.get(`http://api.wordnik.com:80/v4/word.json/${word}/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=${process.env.WORDNIK_API_KEY}`)
     .then((result) => {
@@ -48,16 +48,16 @@ app.get('/synonyms/:word', (req, res) => {
       const test = result.data[0].words.toString();
     // console.log(1, result.data[0].words.toString());
     // console.log(2, test.split(','));
-    //   redisClient.hmset(`${word}_word`, 'synonyms', test);
-    //   redisClient.expire(`${word}_word`, 999999999999999);
+      redisClient.hmset(`${word}_word`, 'synonyms', test);
+      redisClient.expire(`${word}_word`, 999999999999999);
       res.send(result.data[0].words);
     })
     .catch((error) => {
       console.log(error);
       return error;
     });
-//     }
-//   });
+    }
+  });
 });
 
 module.exports = app;
